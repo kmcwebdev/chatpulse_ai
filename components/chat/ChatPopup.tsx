4f054afe07fb56a4670de62dc29aa4d9bd66d7a9
ruin9999 <b14ktoss@gmail.com>
2024-06-20 10:04:33 +0800

    Added responsive design

---
+++ components/chat/ChatPopup.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import { useEffect, useRef, useState } from "react";
+
+export default function ChatPopup(props : {
+	className ?: string;
+	isOpen ?: boolean;
+}) {
+	const [isOpen, setIsOpen] = useState(props.isOpen ? props.isOpen : false);
+	const chatPopupRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		console.log(isOpen);
+	}, [isOpen])
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+				if (chatPopupRef.current && !chatPopupRef.current.contains(event.target as Node)) {
+						setIsOpen(false);
+				}
+		};
+		window.addEventListener('click', handleClickOutside);
+		return () => {
+				window.removeEventListener('click', handleClickOutside);
+		};
+}, []);
+
+	return(<div className="absolute bottom-5 right-5" ref={chatPopupRef}>
+		<div className={`relative flex items-center justify-center h-fit w-fit aspect-square rounded-full bg-accent shadow-2xl p-4`}
+		onClick={() => setIsOpen(true)}
+		>
+	 		<ChatBubbleLeftRightIcon className="size-8 text-white" />
+		</div>
+		<div className={`absolute bottom-7 right-7 overflow-hidden rounded-lg border-black border-2  ${isOpen ? "h-fit" : "h-0 border-0"}`} style={{zIndex: 9999}}>
+			<iframe 
+				id="chatPopup"
+				title="Support Popup"
+				width={300}
+				height={500}
+				src="/live-chat"
+				>
+			</iframe>
+		</div>
+	</div>
+	)
+}
