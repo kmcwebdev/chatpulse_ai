e23ccbd661cdcda484475ac178b44fa38c2f1d07
ruin9999 <b14ktoss@gmail.com>
2024-06-28 00:46:07 +0800

    Added functionality for PDF and Image upload

--- components/chat/ChatBubble.tsx
+++ components/chat/ChatBubble.tsx
@@ -1,24 +1,52 @@
-import ReactMarkdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
+import { IChatMessage } from "@/convex/schema";
+import { calculateTimePassed } from "@/utils/utils";
+import { FolderArrowDownIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 
-interface ConversationBubbleProps {
-	header: string;
-	content: string;
-	ref? : React.RefObject<any>
-	footer?: string;
-	isRight?: boolean;
+interface ChatBubbleProps extends IChatMessage {
+	isRight ?: boolean;
+	ref ?: React.RefObject<any>;
 }
 
-export default function ConversationBubble(props : ConversationBubbleProps) {
-	return(
+export default function ChatBubble(props: ChatBubbleProps) {
+	const renderContent = () => {
+		if(!props.link) return <ReactMarkdown rehypePlugins={[rehypeRaw]}>{props.message}</ReactMarkdown>
+
+		else if(props.format?.includes("image")) {
+			return (
+				<Image
+					alt="image"
+					height={500}
+					width={400}
+					src={props.link}
+					className="max-w-[400px] max-h-[500px] object-contain"
+				/>
+			)
+		} else if(props.format?.includes("pdf")) {
+			return (
+				<Link href={props.link} className="text-white flex space-x-4">
+					<span>{props.message}</span>
+					<FolderArrowDownIcon className="size-6" />
+				</Link>
+			)
+		}
+	}
+
+	return (
 		<div className={`chat text-xs ${props.isRight ? "chat-end" : "chat-start"}`} ref={props.ref}>
-			<div className="chat-header">{props.header}</div>
+			<div className="chat-header">{props.sender}</div>
 			<div className="flex items-center justify-center chat-bubble text-white prose">
-				<ReactMarkdown rehypePlugins={[rehypeRaw]}>
-					{props.content}
-				</ReactMarkdown>
+				{renderContent()}
 			</div>
-			<small className="chat-footer text-[10px] text-black font-light">{props.footer}</small>
+			<small className="chat-footer text-[10px] text-black font-light">
+				{
+					calculateTimePassed(new Date(props.timestamp).getTime().toString())
+				}
+			</small>
 		</div>
 	)
 }
